53a102f4e9c0fcc0d6a08997e503261f1a18d7d4
Spooknik <steven.phillips.17@me.com>
2017-09-29 16:36:56 +0200

    Added subtitle functionality

--- BatchMaker.py
+++ BatchMaker.py
@@ -1,8 +1,14 @@
+# video-transcode-batch-script
+#
+# Copyright (c) 2017 Steven Phillips
+#
 import os
 import sys
+import subprocess
 
-#Set Input Path f.x. C:\movies\
+#Set Input Path f.x. C:\movies NOT C:\movies\
 InputPath = '/absolute/path/to/inputs'
+print('Processing ' + InputPath + '...')
 #Set Output Path. 
 OutputPath = '"/absolute/path/to/outputs"'
 #Set based on your preference. f.x. 'batch.bat' for Windows
@@ -10,10 +16,21 @@ BatchScriptName = 'batch.sh'
 #Set based on which video type you want
 VideoTypeFilter = '.mkv'
 
-with open(BatchScriptName, "w") as a:
-    for path, subdirs, files in os.walk(InputPath):
+SubtitleEngDetect = b'English (iso639-2: eng)' #Must start with 'b' 
+
+with open(BatchScriptName, "w") as a: #Open the script
+    for path, subdirs, files in os.walk(InputPath, topdown=True): #Walk the file path
        for filename in files:
-          if filename.endswith(VideoTypeFilter):
+          if filename.endswith(VideoTypeFilter): #Filter file types
             f = os.path.join(path, filename)
+            print('Now Processing: ' + f)
+            output = subprocess.check_output('transcode-video --scan ' + '"' + str(f) + '"' , shell=True) #Check for subtitles
+            if b'English (iso639-2: eng)' in output: #If English subtitle is detected in scan output
+               EngSubs = ' --add-subtitle eng '
+               print('English subtitle found, adding...')
+            else:
+               EngSubs = ''
+               print('No subtiles found.')
             #Set agruments for transcode-video. Change as needed. Each line must start with 'call' when using Windows. f.x. 'call transcode-video...'
-            a.write('transcode-video --target 1080p=8000 --quick --output ' + OutputPath + ' "' + str(f) + '"' + os.linesep) 
+            a.write('transcode-video' + EngSubs + '--target 1080p=8000 --quick --output ' + OutputPath + ' "' + str(f) + '"' + os.linesep)
+            
