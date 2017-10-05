477708b9dff54c84202f0c8fdc90f2df84ca9404
Spooknik <steven.phillips.17@me.com>
2017-10-05 16:01:00 +0200

    Update 1.1
    Script will now ask for input and output folder before making script.
    Included error checking incase input and output folders are the same.

--- BatchMaker.py
+++ BatchMaker.py
@@ -1,28 +1,35 @@
 # video-transcode-batch-script
-#
 # Copyright (c) 2017 Steven Phillips
 import os
 import sys
 import subprocess
 
-InputPath = 'Z:\Series\Star Trek Voyager\Season 1' #Set Input Path f.x. C:\movies NOT C:\movies\
-print('Input Path: ' + InputPath + '...')
-OutputPath = '"/absolute/path/to/outputs"' #Set Output Path. 
+InputPath = input("Enter the input path of your videos: ")#'Z:\Series\Star Trek Voyager\Season 1' #Set Input Path f.x. C:\movies NOT C:\movies\
+OutputPath = input("Enter the output path of your videos: ") #Set Output Path. 
 BatchScriptName = 'batch.sh' #Set based on your preference. f.x. 'batch.bat' for Windows
 VideoTypeFilter = '.mkv' #Set based on which video type you want
-SubtitleEngDetect = b'English (iso639-2: eng)' #Must start with 'b' 
+SubtitleEngDetect = b'English (iso639-2: eng)' #Must start with 'b'
 
+#Error checking
+assert os.path.exists(InputPath), str(InputPath) + ' is not a valid path.'
+assert os.path.exists(OutputPath), str(OutputPath) + ' is not a valid path.'
+if InputPath == OutputPath:
+   sys.exit('Input path must be different than output path')
+
+print('Using Input Path: ' + InputPath + '...')
 with open(BatchScriptName, "w") as a: #Open the script
     for path, subdirs, files in os.walk(InputPath): #Walk the file path.
        for filename in files:
           if filename.endswith(VideoTypeFilter): #Filter file types by VideoTypeFilter.
-            f = os.path.join(path, filename)
-            print('Now Processing: ' + f)
-            output = subprocess.check_output('transcode-video --scan ' + '"' + str(f) + '"' , shell=True) #Check for subtitles
+            TotalFiles = os.path.join(path, filename)
+            print('Now Processing: ' + str(TotalFiles))
+            output = subprocess.check_output('transcode-video --scan ' + '"' + str(TotalFiles) + '"' , shell=True) #Check for subtitles
             if b'English (iso639-2: eng)' in output: #If English subtitle is detected in scan output. Change 'English' and 'eng' to preferred language.
                EngSubs = ' --add-subtitle eng '
                print('English subtitle found, adding...')
             else:
                EngSubs = ''
                print('No subtiles found.')
-            a.write('transcode-video' + EngSubs + '--target 1080p=8000 --quick --output ' + OutputPath + ' "' + str(f) + '"\n') #Set arguments for transcode-video. Change as needed. Each line must start with 'call' when using Windows. f.x. 'call transcode-video...'
+            a.write('transcode-video' + str(EngSubs) + '--target 1080p=8000 --quick --output ' + str(OutputPath) + ' "' + str(TotalFiles) + '"\n') #Set arguments for transcode-video. Change as needed. Each line must start with 'call' when using Windows. f.x. 'call transcode-video...'
+            print('Completed')
+print('Process completed. ' + BatchScriptName + ' has been written to disk.')
