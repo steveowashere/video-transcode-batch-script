3d0bfd4045703801f59d4905621b08758f6c8e7b
Spooknik <steven.phillips.17@me.com>
2017-10-06 19:07:19 +0200

    Update 1.1. Fix

--- BatchMaker.py
+++ BatchMaker.py
@@ -11,9 +11,9 @@ VideoTypeFilter = '.mkv' #Set based on which video type you want
 SubtitleEngDetect = b'English (iso639-2: eng)' #Must start with 'b'
 
 #Error checking
-assert os.path.exists(InputPath), str(InputPath) + ' is not a valid path.'
+assert os.path.exists(InputPath), str(InputPath) + ' is not a valid path.' 
 assert os.path.exists(OutputPath), str(OutputPath) + ' is not a valid path.'
-if InputPath == OutputPath:
+if InputPath == OutputPath: 
    sys.exit('Input path must be different than output path')
 
 print('Using Input Path: ' + InputPath + '...')
@@ -30,6 +30,6 @@ with open(BatchScriptName, "w") as a: #Open the script
             else:
                EngSubs = ''
                print('No subtiles found.')
-            a.write('transcode-video' + str(EngSubs) + '--target 1080p=8000 --quick --output ' + str(OutputPath) + ' "' + str(TotalFiles) + '"\n') #Set arguments for transcode-video. Change as needed. Each line must start with 'call' when using Windows. f.x. 'call transcode-video...'
+            a.write('transcode-video' + str(EngSubs) + '--target 1080p=8000 --quick --output ' + '"' + str(OutputPath) + '"' + ' "' + str(TotalFiles) + '"\n') #Set arguments for transcode-video. Change as needed. Each line must start with 'call' when using Windows. f.x. 'call transcode-video...'
             print('Completed')
 print('Process completed. ' + BatchScriptName + ' has been written to disk.')
